4e8ec3f30199318cad626e0f1dc485a3d05c68c6
jomer25 <jomer707@gmail.com>
2023-07-01 10:25:32 +0800

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="chat">
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label for="name">What's your name?</label>
+        <input type="text" v-model="name" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    <div class="chat-container" v-else>
+      <div class="messages-container">
+        <div v-for="message in messages" :key="message">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label for="message"
+            ><input type="text" v-model="messageText" @input="emitTyping"
+          /></label>
+
+          <button type="submit">Send</button>
+        </form>
+      </div>
+
+      <div v-if="typingDisplay">{{ typingDisplay }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { io } from "socket.io-client";
+import { onBeforeMount, ref } from "vue";
+
+const socket = io("http://localhost:3000");
+const messages = ref([]);
+const messageText = ref("");
+const joined = ref(false);
+const name = ref("");
+const typingDisplay = ref("");
+onBeforeMount(() => {
+  socket.emit("findAllMessages", {}, (response) => {
+    messages.value = response;
+  });
+
+  socket.on("message", (message) => {
+    messages.value.push(message);
+  });
+
+  socket.on("typing", ({ name, isTyping }) => {
+    isTyping
+      ? (typingDisplay.value = `${name} is typing ...`)
+      : (typingDisplay.value = "");
+  });
+});
+
+const join = () => {
+  socket.emit("join", { name: name.value }, () => {
+    joined.value = true;
+  });
+};
+
+const sendMessage = () => {
+  socket.emit("createMessage", { text: messageText.value }, (response) => {
+    // messages.value.push(response);
+    messageText.value = "";
+  });
+};
+
+let timeout;
+const emitTyping = () => {
+  socket.emit("typing", { isTyping: true });
+  timeout = setTimeout(() => {
+    socket.emit("typing", { isTyping: false });
+  }, 2000);
+};
+</script>
+
+<style scoped>
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.messages-container {
+  flex: 1;
+}
+</style>
